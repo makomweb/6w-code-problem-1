0ce66a9f1c43333961c9e08a48897148446bbf35
Martin Komischke <martin@6wunderkinder.com>
2014-10-27 10:57:33 +0100

    * improve F# solution

--- TweetSearchFSharp/Searcher.fs
+++ TweetSearchFSharp/Searcher.fs
@@ -11,7 +11,7 @@ open Newtonsoft;
 let key = "" // TODO enter your consumer key here!
 let secret = "" // TODO enter your consumer secret here!
 
-let create_basic_auth(key, secret) = 
+let create_basic_auth(key:string, secret:string) = 
     sprintf "%s:%s" key secret
     |> Encoding.UTF8.GetBytes
     |> Convert.ToBase64String
@@ -23,14 +23,14 @@ type credentials = {
     access_token : string
 }
 
-let deserialize_credentials(raw) =
+let deserialize_credentials(raw:string) =
     Json.JsonConvert.DeserializeObject<credentials>(raw)
     
 let get_bearer_token =
     async {
         let http = new HttpClient()
         http.DefaultRequestHeaders.Add("Authorization", "Basic " + basic_auth)
-        let content = new System.Net.Http.StringContent("grant_type=client_credentials", Encoding.UTF8, "application/x-www-form-urlencoded")
+        let content = new StringContent("grant_type=client_credentials", Encoding.UTF8, "application/x-www-form-urlencoded")
         let! response = http.PostAsync("https://api.twitter.com/oauth2/token", content) |> Async.AwaitTask
         response.EnsureSuccessStatusCode |> ignore
         let! s = response.Content.ReadAsStringAsync() |> Async.AwaitTask
@@ -46,11 +46,11 @@ type search_response = {
     statuses : tweet []
 }
 
-let deserialize_tweets(raw) =
+let deserialize_tweets(raw:string) =
     let resp = Json.JsonConvert.DeserializeObject<search_response>(raw)
     resp.statuses
 
-let get_tweets(search_term) =
+let get_tweets(search_term:string) =
     async {
         let http = new HttpClient()
         let! bearer_token = get_bearer_token
